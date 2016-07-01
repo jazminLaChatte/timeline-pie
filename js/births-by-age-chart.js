dc12a5f4a921cef06d4d0d3c85a3393be459cb05
mugdha <mugdha@cdl-00006.local>
2016-07-01 09:27:16 -0700

    basic pie chart

--- js/births-by-age-chart.js
+++ js/births-by-age-chart.js
@@ -14,15 +14,17 @@ var BirthsByAgeChart={
             var obj={
                 year:d[0],
                 total:d[1],
-                age_u15:d[2],
-                age_15to19:d[3],
-                age_20to24:d[4],
-                age_25to29:d[5],
-                age_30to34:d[6],
-                age_35to39:d[7],
-                age_40to44:d[8],
-                age_45u:d[9],
-                age_unknown:d[10]
+                categories:{
+                    age_u15:a[2],
+                    age_15to19:d[3],
+                    age_20to24:d[4],
+                    age_25to29:d[5],
+                    age_30to34:d[6],
+                    age_35to39:d[7],
+                    age_40to44:d[8],
+                    age_45u:d[9],
+                    age_unknown:d[10]
+                }
             };
             that.data.push(obj);
         });
--- js/pie-chart.js
+++ js/pie-chart.js
@@ -1,4 +1,64 @@
-var PieChart = function(data, categories){
-    this.data=data;
-    this.categories=categories;
+var PieChart = function(config){
+    config = config || {};
+    $.extend(this,config);
 };
+
+PieChart.prototype.drawPie = function() {
+    var that=this;
+    //svg
+    var svg = d3.select(that.parentEle)
+        .append("svg")
+        .attr("width", that.chartW)
+        .attr("height", that.chartH)
+        .append("g")
+        .attr("transform", "translate("+that.chartW/2+","+that.chartH/2+")");
+    //calculate arc data
+    var arcData = d3.pie()
+    .value(function(d){
+        return d.value
+    })(that.data);
+    //generate arc elements
+    var arcs = svg.selectAll(".arc")
+        .data(arcData)
+        .enter()
+        .append("g")
+        .attr("class", "arc");
+    //define arc function
+    var arc = d3.arc()
+        .outerRadius(200)
+        .innerRadius(100); //TODO
+    //draw the arcs
+    arcs.append("path")
+          .attr("d", arc)
+          .attr("fill", function(d,i){
+            return that.colors[i];
+          });
+}
+
+function createChart(){
+    var chart = new PieChart({
+        chartW:500,
+        chartH:500,
+        data:  [
+            {
+                name:"A1",
+                value: 10
+            },
+            {
+                name:"A2",
+                value:20
+            },
+            {
+                name:"A3",
+                value:30
+            },
+            {
+                name:"A4",
+                value:40
+            }
+        ],
+        parentEle : $("#thePieChart")[0],
+        colors:["#00ffff", "#00ff00", "#ff00ff", "#ffff00"]
+    });
+    chart.drawPie();
+}
