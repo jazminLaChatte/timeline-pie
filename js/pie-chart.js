ddb399c08e001b105ca72930050c3575889cdf30
mugdha <mugdha@cdl-00006.local>
2016-07-05 08:29:16 -0700

    check

--- js/pie-chart.js
+++ js/pie-chart.js
@@ -16,7 +16,8 @@ PieChart.prototype.drawPie = function() {
     var arcData = d3.pie()
     .value(function(d){
         return d.value
-    })(that.data);
+    }).sortValues(null)
+    (that.data);
     //generate arc elements
     var arcs = svg.selectAll(".arc")
         .data(arcData)
@@ -26,39 +27,56 @@ PieChart.prototype.drawPie = function() {
     //define arc function
     var arc = d3.arc()
         .outerRadius(200)
-        .innerRadius(100); //TODO
+        .innerRadius(75); //TODO
     //draw the arcs
-    arcs.append("path")
+    arcPaths=arcs.append("path")
           .attr("d", arc)
           .attr("fill", function(d,i){
             return that.colors[i];
           });
+
+     setInterval(function() {
+       arcPaths.transition()
+           .duration(750)
+           .call(arcTween, Math.random() * τ);
+     }, 1500);
 }
 
 function createChart(){
     var chart = new PieChart({
         chartW:500,
         chartH:500,
-        data:  [
-            {
+        data:  [{
                 name:"A1",
                 value: 10
-            },
-            {
+            },{
                 name:"A2",
                 value:20
-            },
-            {
+            },{
                 name:"A3",
                 value:30
-            },
-            {
+            },{
                 name:"A4",
+                value:10
+            },{
+                name:"A5",
+                value:20
+            },{
+                name:"A6",
+                value:50
+            },{
+                name:"A7",
+                value:20
+            },{
+                name:"A8",
                 value:40
+            },{
+                name:"A9",
+                value:10
             }
         ],
         parentEle : $("#thePieChart")[0],
-        colors:["#00ffff", "#00ff00", "#ff00ff", "#ffff00"]
+        colors:["#69B242","#42B285", "#4296B2 ", "#4262B2", "#6542B2", "#B2427E", "#B24247", "#B27A42", "#8FB242"]
     });
     chart.drawPie();
 }
