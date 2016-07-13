23c114d426444f698ccc27925030b250b714c870
Mugdha Barve <mbarve@illumina.com>
2016-07-13 09:59:33 -0700

    basic tmeline axis

--- js/pie-chart.js
+++ js/pie-chart.js
@@ -1,6 +1,7 @@
 var PieChart = function(config){
     config = config || {};
-    //init private vars and methods
+    //init vars and methods
+    this.bottomMargin = 40; //bottom margin for drawing timeline
     this.chartW=400;
     this.chartH=400;
     this.isTimeline=false;
@@ -14,7 +15,9 @@ var PieChart = function(config){
     //TODO error check if data exists and this.data.values
 
     //set chart radii if not set by user
-    var minDimension = Math.min(this.chartW, this.chartH);
+    var minDimension = Math.min(this.chartW, this.chartH-this.bottomMargin);
+    this.pieW = minDimension;
+    this.timelineW=this.chartW*0.7;
     if(!this.outerR){
       this.outerR = minDimension/2;
     }
@@ -26,9 +29,14 @@ var PieChart = function(config){
       var totals = this.data.values.map(function(val){
         return val.total;
       });
-      this.outerRScale = d3.scaleLinear() //TODO
+      this.outerRScale = d3.scaleLinear()
         .domain([Math.min(...totals), Math.max(...totals)]) //calculate min and max totals
         .range([this.outerR*0.8 , this.outerR]);
+      //scale for timeline axis
+      var vals=this.data.values;
+      this.timelineAxisScale = d3.scaleLinear()
+        .domain([vals[0].axisValue, vals[vals.length-1].axisValue])
+        .range([-this.timelineW/2, this.timelineW/2]);
     }
 };
 
@@ -45,11 +53,18 @@ PieChart.prototype.drawTimelinePie = function(){
       .attr("width", that.chartW)
       .attr("height", that.chartH);
   this.svg.piechart = this.svg.append("g")   //g for the pie chart
-      .attr("transform", "translate("+that.chartW/2+","+that.chartH/2+")")
+      .attr("transform", "translate("+that.chartW/2+","+that.pieW/2+")")
       .attr("class", "pie-chart")
   this.svg.timeline = this.svg.append("g")   //g for the timeline
-      .attr("transform", "translate("+that.chartW/2+","+that.chartH/2+")")
+      .attr("transform", "translate("+that.chartW/2+","+(that.pieW+(that.bottomMargin/2))+")")
       .attr("class", "timeline");
+  this.svg.timeline
+      .append("rect")
+      .attr("width",this.timelineW)
+      .attr("height", 3)
+      .attr("x", -this.timelineW/2)
+      .attr("y", 0)
+      .attr("class", "time-axis");
   //draw initial pie chart
   //that.drawChart(this.data.values[0].values);
   that.drawChart(this.data.values[0], true);
@@ -70,6 +85,21 @@ PieChart.prototype.drawChart = function(obj, isFirst) {
     return;
   }
     var that=this;
+    //draw timeline axis indicator
+    var axis = that.svg.timeline
+      .selectAll(".timeline-indicator")
+      .data([obj.axisValue]);
+    axis.enter()
+      .append("circle")
+      .attr("cy",1)
+      .attr("r", 4)
+      .attr("class", "timeline-indicator");
+    this.svg.timeline.selectAll(".timeline-indicator")
+      .attr("cx", function(d){
+        return that.timelineAxisScale(d);
+      });
+    axis.exit().remove();
+
     var arcData = that.calculateArcData(pieValues); //calculate arc data
     var arc = d3.arc()  //define arc function
         .innerRadius(that.innerR);
@@ -118,6 +148,7 @@ PieChart.prototype.drawChart = function(obj, isFirst) {
       });
 };
 
+//test function
 function createChart(){
     var chart = new PieChart({
         chartW:500,
@@ -125,13 +156,16 @@ function createChart(){
         data:  {
           categories:["A", "B", "C", "D", "E"],
           values: [{
+            axisValue:1990,
             total: 180,
             values:[20,30,50,20,60]
           },{
+            axisValue: 2000,
             total: 75,
             values:[10,20,30,10,5]
           },
           {
+            axisValue: 2010,
             total: 100,
             values:[1,20,30,40,9]
           }
@@ -142,3 +176,7 @@ function createChart(){
     });
     chart.drawTimelinePie();
 };
+
+function doTest(){
+  createChart();
+}
